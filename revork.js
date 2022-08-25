4911354cc96b2f53440ed28da6fc8d6cc6abc234
Zerosha <soldatumora@gmail.com>
2022-08-25 17:47:01 +0300

    Fix problems and implementation esc action

--- revork.js
+++ revork.js
@@ -1,3 +1,25 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+  subscribe(eventName, callback) {
+    if (!this.events[eventName] && (this.events[eventName] = [])) {
+      this.events[eventName].push(callback);
+    }
+  }
+  unsubscribe(eventName, callback) {
+    this.events[eventName] = this.events[eventName].filter(
+      (eventCallback) => callback !== eventCallback
+    );
+  }
+  emit(eventName, args) {
+    const event = this.events[eventName];
+    if (event) {
+      event.forEach((callback) => callback.call(null, ...args));
+    }
+  }
+}
+
 class Storage {
   constructor(storage, index) {
     this._storage = storage;
@@ -12,113 +34,81 @@ class Storage {
   set index(value) {
     this._index = value;
   }
-  log(){
+  log() {
     console.log(this._storage);
+  }
+  add(textBox) {
+    if (this.storage.length !== 0) {
+      this.storage.push({
+        id: this.storage[this.storage.length - 1].id + 1,
+        content: textBox.value,
+        completed: false,
+      });
+    } else {
+      this.storage.push({
+        id: 0,
+        content: textBox.value,
+        completed: false,
+      });
+    }
+  }
+  complete(lis, e) {
+    lis.forEach((element, i) => {
+      if (element.textContent == e.target.nextSibling.textContent) {
+        this.index = i;
+      }
+    });
+    if (e.target.nextSibling.nextSibling.disabled === false) {
+      this.storage.forEach((item, i) => {
+        if (i == this.index) {
+          item.completed = true;
+        }
+      });
+    } else {
+      this.storage.forEach((item, i) => {
+        if (i == this.index) {
+          item.completed = false;
+        }
+      });
+    }
+  }
+  redact(lis, e, ...args) {
+    if (e.target.previousSibling.classList.contains("content")) {
+      lis.forEach((element, i) => {
+        if (element.textContent == e.target.previousSibling.textContent) {
+          this.index = i;
+        }
+      });
+    } else {
+      this.storage.forEach((item, i) => {
+        if (i == this.index) {
+          item.content = args[0].textContent;
+        }
+      });
+    }
+  }
+  delete(lis, e) {
+    lis.forEach((element, i) => {
+      if (
+        element.textContent ==
+        e.target.previousSibling.previousSibling.textContent
+      ) {
+        this.index = i;
+      }
+    });
+    this.storage.forEach((item, i) => {
+      if (i == this.index) {
+        this.storage.splice(i, 1);
+      }
+    });
   }
 }
 
 class Task {
-  constructor(ul, storage) {
-    ul.addEventListener("click", (e) => {
-      if (e.target.classList.contains("doneBtn")) {
-        const lis = document.querySelectorAll("li div");
-        lis.forEach((element, i) => {
-          if (element.textContent == e.target.nextSibling.textContent) {
-            storage.index = i;
-          }
-        });
-
-        e.target.nextSibling.classList.toggle("done");
-        if (e.target.nextSibling.nextSibling.disabled === false) {
-          e.target.nextSibling.nextSibling.disabled = true;
-          e.target.textContent = "Доделать";
-          storage.storage.forEach((item, i) => {
-            if (i == storage.index) {
-              item.completed = true;
-            }
-          });
-        } else {
-          e.target.nextSibling.nextSibling.disabled = false;
-          e.target.textContent = "Выполнено";
-          storage.storage.forEach((item, i) => {
-            if (i == storage.index) {
-              item.completed = false;
-            }
-          });
-        }
-      }
-      if (
-        e.target.classList.contains("changeBtn") &&
-        !e.target.previousSibling.classList.contains("done")
-      ) {
-        if (e.target.previousSibling.classList.contains("content")) {
-          const lis = document.querySelectorAll("li div");
-          lis.forEach((element, i) => {
-            if (element.textContent == e.target.previousSibling.textContent) {
-              storage.index = i;
-            }
-          });
-          const redBox = document.createElement("input");
-          redBox.value = e.target.previousSibling.textContent;
-          e.target.previousSibling.replaceWith(redBox);
-          const btn = e.target;
-          e.target.previousSibling.addEventListener("keydown", (e) => {
-            if (e.key === "Enter") {
-              const event = new Event("click", { bubbles: true });
-              btn.dispatchEvent(event);
-            }
-          });
-
-          e.target.textContent = "Сохранить изменения";
-          e.target.previousSibling.previousSibling.disabled = true;
-          e.target.nextSibling.disabled = true;
-        } else {
-          e.target.previousSibling.value =
-            e.target.previousSibling.value.trim();
-          if (e.target.previousSibling.value.length != 0) {
-            const redContent = document.createElement("div");
-            redContent.classList.add("content");
-            redContent.textContent = e.target.previousSibling.value;
-            redContent.addEventListener("click", () => {
-              const event = new Event("click", { bubbles: true });
-              e.target.dispatchEvent(event);
-            });
-            e.target.previousSibling.replaceWith(redContent);
-            e.target.textContent = "Изменить";
-            e.target.previousSibling.previousSibling.disabled = false;
-            e.target.nextSibling.disabled = false;
-            storage.storage.forEach((item, i) => {
-              if (i == storage.index) {
-                item.content = redContent.textContent;
-              }
-            });
-          } else {
-            alert("Заметка должна что-то содержать!");
-          }
-        }
-      }
-      if (e.target.classList.contains("deconsteBtn")) {
-        const lis = document.querySelectorAll("li div");
-        let index = 0;
-        lis.forEach((element, i) => {
-          if (
-            element.textContent ==
-            e.target.previousSibling.previousSibling.textContent
-          ) {
-            index = i;
-          }
-        });
-
-        storage.storage.forEach((item, i) => {
-          if (i == index) {
-            storage.storage.splice(i, 1);
-          }
-        });
-
-        e.target.parentNode.remove();
-      }
-      storage.log();
-    });
+  constructor(eventStorage) {
+    eventStorage.subscribe("done", this.done);
+    eventStorage.subscribe("edit", this.edit);
+    eventStorage.subscribe("delete", this.delete);
   }
   addNewPost(textBox, ul, storage) {
     const li = document.createElement("li");
@@ -147,31 +137,83 @@ class Task {
     li.append(deconsteBtn);
     ul.append(li);
 
-    if (storage.storage.length !== 0) {
-      storage.storage.push({
-        id: storage.storage[storage.storage.length - 1].id + 1,
-        content: textBox.value,
-        completed: false,
-      });
-    } else {
-      storage.storage.push({
-        id: 0,
-        content: textBox.value,
-        completed: false,
-      });
-    }
+    storage.add(textBox);
 
     textBox.value = "";
+  }
+  done(storage, e) {
+    const lis = document.querySelectorAll("li div");
+    e.target.nextSibling.classList.toggle("done");
+    storage.complete(lis, e);
+    if (e.target.nextSibling.nextSibling.disabled === false) {
+      e.target.nextSibling.nextSibling.disabled = true;
+      e.target.textContent = "Доделать";
+    } else {
+      e.target.nextSibling.nextSibling.disabled = false;
+      e.target.textContent = "Выполнено";
+    }
+  }
+  delete(storage, e) {
+    const lis = document.querySelectorAll("li div");
+    storage.delete(lis, e);
+
+    e.target.parentNode.remove();
+  }
+  edit(storage, e) {
+    if (e.target.previousSibling.classList.contains("content")) {
+      const lis = document.querySelectorAll("li div");
+      storage.redact(lis, e);
+      const redBox = document.createElement("input");
+      redBox.value = e.target.previousSibling.textContent;
+      e.target.previousSibling.replaceWith(redBox);
+      let memory=redBox.value;
+      const btn = e.target;
+      e.target.previousSibling.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+          const event = new Event("click", { bubbles: true });
+          btn.dispatchEvent(event);
+        }
+        if(e.key==="Escape"){
+          e.target.value=memory;
+          e.target.blur();
+          const event = new Event("click", { bubbles: true });
+          btn.dispatchEvent(event);
+        }
+      });
+
+      e.target.textContent = "Сохранить изменения";
+      e.target.previousSibling.previousSibling.disabled = true;
+      e.target.nextSibling.disabled = true;
+    } else {
+      e.target.previousSibling.value = e.target.previousSibling.value.trim();
+      if (e.target.previousSibling.value.length != 0) {
+        const redContent = document.createElement("div");
+        redContent.classList.add("content");
+        redContent.textContent = e.target.previousSibling.value;
+        storage.redact(1, e, redContent);
+        redContent.addEventListener("click", () => {
+          const event = new Event("click", { bubbles: true });
+          e.target.dispatchEvent(event);
+        });
+        e.target.previousSibling.replaceWith(redContent);
+        e.target.textContent = "Изменить";
+        e.target.previousSibling.previousSibling.disabled = false;
+        e.target.nextSibling.disabled = false;
+      } else {
+        alert("Заметка должна что-то содержать!");
+      }
+    }
   }
 }
 
 class Form {
   constructor() {
+    const eventStorage = new EventEmitter();
     const textBox = document.createElement("input");
     const addBtn = document.createElement("button");
     const ul = document.createElement("ul");
     const storage = new Storage([], 0);
-    const taskList = new Task(ul, storage);
+    const taskList = new Task(eventStorage);
 
     addBtn.textContent = "Добавить запись";
     textBox.placeholder = "Введи заметку";
@@ -185,11 +227,41 @@ class Form {
         alert("Нечего добавить, введи заметку!");
       }
     });
+    textBox.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        const event = new Event("click", { bubbles: true });
+        addBtn.dispatchEvent(event);
+      }
+      if(e.key==="Escape"){
+        e.target.value="";
+        e.target.blur();
+      }
+    });
+
+    ul.addEventListener("click", (e) => {
+      if (e.target.classList.contains("doneBtn")) {
+      
+        eventStorage.emit("done",[storage,e]);
+      }
+      if (
+        e.target.classList.contains("changeBtn") &&
+        !e.target.previousSibling.classList.contains("done")
+      ) {
+       
+        eventStorage.emit("edit",[storage,e]);
+      }
+      if (e.target.classList.contains("deconsteBtn")) {
+       
+        eventStorage.emit("delete",[storage,e]);
+      }
+      storage.log();
+    });
 
     document.body.append(textBox);
     document.body.append(addBtn);
     document.body.append(ul);
   }
+
 }
 
 const form = new Form();
